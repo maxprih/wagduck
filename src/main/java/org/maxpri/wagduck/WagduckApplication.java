6424c2eb34dffd20272658d6fa5dd6732c304651
maxprih <thesupermaks@gmail.com>
2025-01-24 18:02:46 +0300

    🐤WagDuck init🐤

---
+++ src/main/java/org/maxpri/wagduck/WagduckApplication.java
@@ -0,0 +1,13 @@
+package org.maxpri.wagduck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WagduckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WagduckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/maxpri/wagduck/WagduckApplicationTests.java
@@ -0,0 +1,13 @@
+package org.maxpri.wagduck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WagduckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
